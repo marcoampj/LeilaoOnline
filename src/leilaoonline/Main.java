b73b2195d2458960ae7bfa55e2109c89a75abf6e
marco aurelio <marcoampjr@gmail.com>
2020-04-04 10:56:34 -0300

    Atualização da classe produto

--- src/leilaoonline/Main.java
+++ src/leilaoonline/Main.java
@@ -1,14 +1,13 @@
 package leilaoonline;
 
 public class Main {
-    double Valor;
 
     public static void main(String[] args) {
      produto p1 = new produto();
-     p1.nome = "Notebook Dell I5";
-     p1.valor = 800;
-     p1.codigo = 1313;
+     p1.nome = "Dell I5, 1TB";
      p1.cor = "Preto";
-     p1.status();     
+     p1.codigo = 1313;
+     p1.valor = 800;
+     p1.status();
     }
   }
--- src/leilaoonline/produto.java
+++ src/leilaoonline/produto.java
@@ -3,18 +3,32 @@ package leilaoonline;
 public class produto {
 String nome;
 String cor;
-double valor;
-int codigo;
+ double valor;
+ int codigo;
+ 
+ public String getNome(){
+     return this.nome;
+ }
+ public void setNome(){
+    String n = null;
+     this.nome = n;
+ }
 
+  public String getCor(){
+     return this.cor;
+ }
+ public void setCor(){
+    String c = null;
+     this.cor = c;
+ }
 void status(){
+    System.out.println("Sobre o Produto:");
     System.out.println("Nome: " + this.nome);
     System.out.println("Cor: " + this.cor);
     System.out.println("Valor: " + this.valor);
     System.out.println("Código: " + this.codigo);
-}
-    public produto() {
-                
-        
     }
-   
+    public produto() {
+        
+                       }
 }
